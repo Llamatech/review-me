0378bf08e54ba4259df8590dd2075b0ccf3c338e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-03-04 23:04:56 -0500

    Add layout to perform Github API information request

--- backEnd/models/project.js
+++ backEnd/models/project.js
@@ -3,6 +3,7 @@
 const mongo = require('../db');
 const request = require('request');
 var assert = require('assert');
+var urljoin = require('url-join');
 
 var collection = 'projects';
 
@@ -16,14 +17,37 @@ exports.list = (req, res) => {
 exports.create = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     console.log(req.body);
+
+    // See docs/project.json to see example body
     var project = req.body;
 
-    //Github Processing goes here.
+    // Expected: https://github.com/owner/repo
+    var githubUrl = project.url;
+    var urlObj = url.parse(githubUrl, false, true);
+    var path = url_obj.pathname;
+    var routes = path.split('/');
+    var owner = routes[0];
+    var repo = routes[1];
 
-    mongo.insert('projects', project, (doc) => {
-        res.send(JSON.stringify({ project: doc }));
+    //Github Processing goes here.
+    var apiEndpoint = 'https://api.github.com/repos/';
+    var apiUrl = urljoin(apiEndpoint, owner, repo);
+    request.get(apiUrl, (error, resp, body) => {
+        // Body should comply with the JSON form present on docs/repo.json
+        if(err) throw err;
+        if(resp.statusCode !== 200) {
+            res.status(resp.statusCode).send();
+        }
+        else {
+            mongo.insert('projects', project, (doc) => {
+                res.send(JSON.stringify({ project: doc }));
+            });
+        }
     });
+
 }
+
+
 
 
 
