af309da7e04b64be8e8a509826458f5d542605b3
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-03-06 10:15:20 -0500

    Minify bundle.js test

--- frontEnd/webpack.config.js
+++ frontEnd/webpack.config.js
@@ -1,8 +1,15 @@
 const webpack = require('webpack');
 
+var path = require('path');
 
+// var nodeEnv = process.env.NODE_ENV || 'development';
+// var isProd = nodeEnv === 'production';
+
+var nodeEnv = process.env.NODE_ENV || 'development';
+var isProd = nodeEnv === 'production';
 
 module.exports = {
+  devtool: isProd ? 'cheap-module-source-map' : 'eval',
   entry: [
     './src/index.js'
   ],
@@ -28,6 +35,22 @@ module.exports = {
     contentBase: './'
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      sourceMap: false
+    }),
     new webpack.DefinePlugin({
       'process.env':{
         BACK_URL: '"'+process.env.BACK_URL+'"'
