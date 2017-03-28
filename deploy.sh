e5ec8e4c2f3e033e17b93542fef60697e709b156
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-03-27 22:51:51 -0500

    Minor README modiication

--- deploy.sh
+++ deploy.sh
@@ -2,32 +2,32 @@
 
 printf "Deploying App...\n"
 
-POE="server.js"
-POE2="webpack-dev-server.js"
+POE="meteor"
+# POE2="webpack-dev-server.js"
 
 PR= echo | ps ax | grep $POE | grep -v grep | awk '{print $1}' | xargs kill > /dev/null 2>&1
-PR2= echo | ps ax | grep $POE2 | grep -v grep | awk '{print $1}' | xargs kill > /dev/null 2>&1
+# PR2= echo | ps ax | grep $POE2 | grep -v grep | awk '{print $1}' | xargs kill > /dev/null 2>&1
 
-printf "\nDeploying backend...\n"
-cd backEnd
+printf "\nDeploying...\n"
+# cd backEnd
 printf "\nDownloading dependencies\n"
 
-npm install
+meteor npm install
 nohup npm start > "$POE.out" 2> "$POE.err" &
 
 printf "\nProcess PID: "
 echo $!
 
-printf "\nDeploying frontend...\n"
-cd ../frontEnd
-printf "\nDownloading dependencies\n"
+# printf "\nDeploying frontend...\n"
+# cd ../frontEnd
+# printf "\nDownloading dependencies\n"
 
-export NODE_ENV=production
-npm install
-nohup npm start > "$POE2.out" 2> "$POE2.err" &
+# export NODE_ENV=production
+# npm install
+# nohup npm start > "$POE2.out" 2> "$POE2.err" &
 
-printf "\nProcess PID: "
-echo $!
+# printf "\nProcess PID: "
+# echo $!
 
 
 
