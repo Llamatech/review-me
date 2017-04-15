775a856a741bbaa767afe30463a2878f7aa54aa5
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-14 23:49:09 -0500

    Script update

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 meteor test --once --driver-package dispatch:mocha
+STATUS_CODE=$?
+echo $STATUS_CODE
 if [ $? -eq 0 ]; then
     printf "Calcluating coverage...\n"
     awk 'NR==20 {$0="      \047phantomjs-options\047: \047--load-images=no --ssl-protocol=TLSv1 --remote-debugger-port=9000 --remote-debugger-autorun=yes \047"} { print }' node_modules/spacejam/lib/Spacejam.coffee > node_modules/spacejam/lib/Spacejam.coffee.temp
@@ -8,4 +10,6 @@ if [ $? -eq 0 ]; then
     printf "meteor npm run coverage-app-unit"
     timeout 240s meteor npm run coverage-app-unit
     printf "Done!\n"
+else
+    exit $STATUS_CODE
 fi
