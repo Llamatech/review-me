c3f1bd9e8dd4e50cf25831d20abf2c75fafb648e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-14 23:13:35 -0500

    Added identation

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-awk 'NR==20 {$0="\047phantomjs-options\047: \047--load-images=no --ssl-protocol=TLSv1 --debug=true --remote-debugger-port=9000 --remote-debugger-autorun=yes \047"} { print }' node_modules/spacejam/lib/Spacejam.coffee > node_modules/spacejam/lib/Spacejam.coffee.temp
+awk 'NR==20 {$0="      \047phantomjs-options\047: \047--load-images=no --ssl-protocol=TLSv1 --debug=true --remote-debugger-port=9000 --remote-debugger-autorun=yes \047"} { print }' node_modules/spacejam/lib/Spacejam.coffee > node_modules/spacejam/lib/Spacejam.coffee.temp
 mv node_modules/spacejam/lib/Spacejam.coffee.temp node_modules/spacejam/lib/Spacejam.coffee
 timeout 240s meteor npm run coverage-app-unit
