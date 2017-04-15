d6bc1b3ab91d72a5881f0ccdca907d0633163f6f
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-14 23:32:36 -0500

    circle.yml update

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-awk 'NR==20 {$0="      \047phantomjs-options\047: \047--load-images=no --ssl-protocol=TLSv1 --debug=true --remote-debugger-port=9000 --remote-debugger-autorun=yes \047"} { print }' node_modules/spacejam/lib/Spacejam.coffee > node_modules/spacejam/lib/Spacejam.coffee.temp
-mv node_modules/spacejam/lib/Spacejam.coffee.temp node_modules/spacejam/lib/Spacejam.coffee
-timeout 240s meteor npm run coverage-app-unit
+meteor test --once --driver-package dispatch:mocha
+if [ $? -eq 0 ]; then
+    printf "Calcluating coverage...\n"
+    awk 'NR==20 {$0="      \047phantomjs-options\047: \047--load-images=no --ssl-protocol=TLSv1 --remote-debugger-port=9000 --remote-debugger-autorun=yes \047"} { print }' node_modules/spacejam/lib/Spacejam.coffee > node_modules/spacejam/lib/Spacejam.coffee.temp
+    mv node_modules/spacejam/lib/Spacejam.coffee.temp node_modules/spacejam/lib/Spacejam.coffee
+    printf "meteor npm run coverage-app-unit"
+    timeout 240s meteor npm run coverage-app-unit
+    printf "Done!\n"
+fi
