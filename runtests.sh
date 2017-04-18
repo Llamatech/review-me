280e34464c3c0bbf9ca4819da26d5f5845195f06
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-17 22:16:18 -0500

    Force test repeat

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 meteor test --once --driver-package dispatch:mocha
+meteor test --once --driver-package dispatch:mocha
 STATUS_CODE=$?
-echo $STATUS_CODE
+# echo $STATUS_CODE
 if [ $? -eq 0 ]; then
     printf "Calcluating coverage...\n"
     awk 'NR==20 {$0="      \047phantomjs-options\047: \047--load-images=no --ssl-protocol=TLSv1 --remote-debugger-port=9000 --remote-debugger-autorun=yes \047"} { print }' node_modules/spacejam/lib/Spacejam.coffee > node_modules/spacejam/lib/Spacejam.coffee.temp
