77caab4c1a964c6eddbfb9fb6f68bbb3b27590cc
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-18 17:52:31 -0500

    More tests added

--- imports/ui/proyecto/proyecto.test.jsx
+++ imports/ui/proyecto/proyecto.test.jsx
@@ -70,7 +70,7 @@ if(Meteor.isClient)
                     'ratings' : {
                         'avgRate': 3,
                     },
-                    'user' : 'andfoy'
+                    'user' : 'Llamatest'
             });
 
             const result  = shallow(<Proyecto id={testProject._id} proyecto={testProject} /> );
@@ -78,7 +78,7 @@ if(Meteor.isClient)
           chai.assert(result.hasClass('proyecto'));
         });
 
-        it("Should render a partial project", function() {
+        it("Should render a disowned project", function() {
             const testProject = Factory.build('project', {
                     'url' : 'https://github.com/Llamatech/review-me',
                     'description' : '',
@@ -117,6 +117,56 @@ if(Meteor.isClient)
             console.log(stars_wrapper.debug());
             chai.assert.equal(info_header.children().at(0).text(), testProject.name);
             chai.assert.equal(info_header.children().at(1).text(), testProject.owner);
+            chai.assert.equal(summary_wrapper.text(), testProject.summary);
+            chai.assert.equal(url_wrapper.childAt(0).props().href, testProject.url);
+            chai.assert.equal(repo_stats.childAt(0).text(), 'Forks: ' + testProject.repo.forks);
+            chai.assert.equal(repo_stats.childAt(1).text(), 'Stars: ' + testProject.repo.stars);
+            chai.assert.equal(repo_stats.childAt(2).text(), 'Watches: ' + testProject.repo.watchers);
+            chai.assert.equal(stars_wrapper.childAt(0).props().value, testProject.ratings.avgRate);
+            // chai.assert(false);
+        });
+
+        it("Should render an owned project", function() {
+            const testProject = Factory.build('project', {
+                    'url' : 'https://github.com/Llamatech/review-me',
+                    'description' : 'Lorem',
+                    'collaborator' : 'Ipsum',
+                    'id' : 83160698,
+                    'name' : 'review-me',
+                    'owner' : 'Llamatech',
+                    'summary' : 'The Internet Project Database - A system to review and rate FOSS projects hosted on Github',
+                    'webpage' : 'http://review-me.margffoy-tuay.com',
+                    'repo' : {
+                        'url' : 'https://github.com/Llamatech/review-me',
+                        'fork' : false,
+                        'watchers' : 0,
+                        'forks' : 8,
+                        'stars' : 0,
+                        'language' : 'JavaScript',
+                        'issues' : 3,
+                    },
+                    'parent_repo' : '',
+                    'comments' : [],
+                    'ratings' : {
+                        'avgRate': 3,
+                    },
+                    'user' : 'Llamatest'
+            });
+
+            const wrapper  = shallow(<Proyecto id={testProject._id} proyecto={testProject} /> );
+            const well = wrapper.find(Well);
+            // console.log(well.debug());
+            const info_wrapper = well.children().children();
+            const info_header = info_wrapper.at(0);
+            const summary_wrapper = info_wrapper.at(1);
+            const url_wrapper = info_wrapper.at(2);
+            const repo_stats = info_wrapper.at(3);
+            const stars_wrapper = info_wrapper.at(4);
+            // console.log(well.debug());
+            // console.log(info_header.childAt(0).debug());
+            chai.assert.equal(info_header.childAt(0).childAt(0).text(), '×');
+            chai.assert.equal(info_header.children().at(1).text(), testProject.name);
+            chai.assert.equal(info_header.children().at(2).text(), testProject.owner);
             chai.assert.equal(summary_wrapper.text(), testProject.summary);
             chai.assert.equal(url_wrapper.childAt(0).props().href, testProject.url);
             chai.assert.equal(repo_stats.childAt(0).text(), 'Forks: ' + testProject.repo.forks);
